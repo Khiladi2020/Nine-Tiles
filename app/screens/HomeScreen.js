7c7f9f87ab7fafb66ac8f893ef128d860f0df571
Ravinder Singh <ravindermaan0123@gmail.com>
2022-05-13 23:15:18 +0530

    Created Home Screen Component

---
+++ app/screens/HomeScreen.js
@@ -0,0 +1,65 @@
+import React, {useState} from 'react';
+import {View, StyleSheet, Alert, Button, ToastAndroid} from 'react-native';
+
+import AppScreen from '../components/AppScreen';
+import AppText from '../components/AppText';
+import AppButton from '../components/AppButton';
+
+const HomeScreen = props => {
+  const startGame = () => {
+    props.startGame();
+  };
+
+  return (
+    <AppScreen style={styles.container}>
+      <View style={styles.form}>
+        <View style={styles.main}>
+          <View>
+            <AppText style={styles.heading}>Select number</AppText>
+          </View>
+          <View style={styles.inputs}>
+            <AppButton title=" ADD " onPress={props.increaseCount} />
+            <AppText style={styles.guessText}>{props.guessNumber}</AppText>
+            <AppButton title=" DEC " onPress={props.decreaseCount} />
+          </View>
+        </View>
+        <AppButton title="Start Game" onPress={props.startGame} />
+      </View>
+    </AppScreen>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  heading: {
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+  main: {
+    height: '70%',
+    width: '100%',
+    alignItems: 'center',
+  },
+  inputs: {
+    width: '100%',
+    paddingTop: 40,
+    // backgroundColor:'red',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  form: {
+    width: '100%',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 30,
+  },
+  guessText: {
+    fontSize: 150,
+  },
+});
+
+export default HomeScreen;
