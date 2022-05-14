b9b458dc498db94eb5e5922d9f20ccf5709b8e45
Ravinder Singh <ravindermaan0123@gmail.com>
2022-05-14 20:32:45 +0530

    refactored GameScreen UI

--- app/screens/GameScreen.js
+++ app/screens/GameScreen.js
@@ -4,30 +4,38 @@ import {View, StyleSheet, TouchableNativeFeedback, Alert} from 'react-native';
 import AppButton from '../components/AppButton';
 import AppScreen from '../components/AppScreen';
 import AppText from '../components/AppText';
-import Tile from '../components/Tile'
+import Tile from '../components/Tile';
+
+const getStatusMessage = gameStatus => {
+  let status_msg;
+
+  if (gameStatus === 'PROGRESS') status_msg = 'Game is in Progress';
+  else if (gameStatus === 'WIN') status_msg = '🎉 You have won! 🥳';
+  else status_msg = '😞 You have lost!';
+
+  return status_msg;
+};
 
 const GameScreen = props => {
+  const status_msg = getStatusMessage(props.gameStatus);
+
   return (
-    <AppScreen style={styles.container}>
+    <AppScreen>
       <View style={styles.backNavigation}>
         <AppButton title="Go Back" onPress={() => props.changeScreen('HOME')} />
-        <View style={{flex: 1, alignItems: 'flex-end'}}>
-          {props.gameStatus != 'PENDING' && (
-            <AppButton title="Reveal Cards" onPress={props.revealAllCards} />
+        <View style={styles.revealCard}>
+          {props.gameStatus != 'PROGRESS' && (
+            <AppButton title="Reveal Cards" onPress={props.revealAllTiles} />
           )}
         </View>
-        {/* <View style={{flex:1}}/> */}
       </View>
       <View style={styles.main}>
-        <View style={{paddingVertical: 30, flex: 1, justifyContent: 'center'}}>
-          {['WON', 'LOST'].includes(props.gameStatus) ? (
-            <AppText>You have {props.gameStatus}!</AppText>
-          ) : (
-            <>
-              <AppText>Number to Guess: {props.guessNumber}</AppText>
-              <AppText>Chances Remaining: {props.chancesRemaining}</AppText>
-            </>
-          )}
+        <View style={styles.statusView}>
+          <AppText style={styles.statusMessage}>{status_msg}</AppText>
+        </View>
+        <View style={styles.gameStats}>
+          <AppText>Guess: {props.guessNumber}</AppText>
+          <AppText>Lives: {props.livesRemaining}</AppText>
         </View>
         <View style={styles.tiles}>
           {props.tiles.map(ele => (
@@ -37,7 +45,7 @@ const GameScreen = props => {
               title={ele.title}
               value={ele.value}
               showData={ele.revealed}
-              onClick={props.onRevealCard}
+              onClick={props.onTileClick}
             />
           ))}
         </View>
@@ -47,28 +55,38 @@ const GameScreen = props => {
 };
 
 const styles = StyleSheet.create({
-  container: {
-    // justifyContent: 'center',
-    alignItems: 'center',
-  },
   backNavigation: {
     width: '100%',
     flexDirection: 'row',
     paddingTop: 10,
     paddingHorizontal: 15,
   },
-  main: {
+  revealCard: {
+    flex: 1,
+    alignItems: 'flex-end',
+  },
+  statusView: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    // backgroundColor:'violet'
+  },
+  statusMessage: {
+    fontSize: 23,
+    fontWeight: 'bold',
+  },
+  gameStats: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    paddingBottom: 15,
+  },
+  main: {
+    flex: 1,
   },
   tiles: {
-    // flex: 1,
     flexDirection: 'row',
     flexWrap: 'wrap',
     justifyContent: 'center',
-    paddingBottom: 30,
+    paddingBottom: 25,
   },
 });
 
